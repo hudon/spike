4c75e190c6725c4f11b5f6b35b2f9bcd268bc0c5
James Hudon <jameshudon@gmail.com>
2013-06-11 16:46:40 -0400

    prepare test suite for spike flag

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,21 +7,19 @@ PYTHON=$(command -v python2 || command -v python2.7 || command -v python)
 THIS_SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #  All file locations are relative to this file
-CODE_TO_TEST_PATH="../src"
-WORKING_CODE_PATH="../examples/new-theano"
+TARGET_DIR="../src"
+SOURCE_DIR="../examples/new-theano"
 NENGO_TESTS_DIR="nengo_tests"
 
 TEST_SCRIPTS=(
   "${NENGO_TESTS_DIR}/test_array.py"
-#"${NENGO_TESTS_DIR}/test_cleanup.py"
-#"${NENGO_TESTS_DIR}/test_decoded_weight_matrix.py"
 );
 
 compareOutput(){
   ACTUAL_OUT_CMD="${PYTHON} ${THIS_SCRIPT_DIRECTORY}/${1}\
-    ${THIS_SCRIPT_DIRECTORY}/${CODE_TO_TEST_PATH}"
+    ${THIS_SCRIPT_DIRECTORY}/${TARGET_DIR} tt"
   EXPECTED_OUT_CMD="${PYTHON}  ${THIS_SCRIPT_DIRECTORY}/${1}\
-    ${THIS_SCRIPT_DIRECTORY}/${WORKING_CODE_PATH}"
+    ${THIS_SCRIPT_DIRECTORY}/${SOURCE_DIR}"
 
   #  This will compare the output that goes to standard out.  At the moment, we don't
   #  check the output on standard error.
@@ -44,18 +42,18 @@ compareOutput(){
   diff=$(diff <(echo "${ACTUAL_OUT}") <(echo "${EXPECTED_OUT}"))
 
   if [ ${#diff} -ne 0 ]; then
-    echo "Output from '${ACTUAL_OUT_CMD}' and '${EXPECTED_OUT_CMD}' does not match."
-    echo -e "Diff was:"
+    echo -e "ERROR: Diff was:"
     echo "$diff"
-    echo -e ${RED_TEXT}"FAILURE:"${NORMAL_TEXT}
+    echo -e ${RED_TEXT}"ERROR: Output from '${ACTUAL_OUT_CMD}' and \
+      '${EXPECTED_OUT_CMD}' does not match."${NORMAL_TEXT}
     exit 1
   elif [ ${ACTUAL_RETURN_CODE} -ne ${EXPECTED_RETURN_CODE} ]; then
-    echo -e ${RED_TEXT}"FAILURE:"${NORMAL_TEXT}
-    echo "Return code of ${ACTUAL_RETURN_CODE} from '${ACTUAL_OUT_CMD}' and ${EXPECTED_RETURN_CODE} from '${EXPECTED_OUT_CMD}' does not match."
-    echo "Test failure."
+    echo -e ${RED_TEXT}"ERROR:"${NORMAL_TEXT}
+    echo "Return code of ${ACTUAL_RETURN_CODE} from '${ACTUAL_OUT_CMD}' \
+      and ${EXPECTED_RETURN_CODE} from '${EXPECTED_OUT_CMD}' does not match."
     exit 1
   else
-    echo -e ${GREEN_TEXT}"Outputs match exactly. Test Passed."${NORMAL_TEXT}
+    echo -e ${GREEN_TEXT}"INFO: All Tests Passed."${NORMAL_TEXT}
   fi
 }
 
