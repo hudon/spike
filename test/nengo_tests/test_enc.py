74c1053d8bd415d206fe13bda0ccbb67bcf0cf73
Robert Elder <robert@robertelder.ca>
2013-07-16 16:48:09 -0400

    Added test_enc to unit tests.

--- test/nengo_tests/test_enc.py
+++ test/nengo_tests/test_enc.py
@@ -6,13 +6,17 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from .. import nef_theano as nef
+import sys
+sys.path.append(sys.argv[1])
+import nef_theano as nef
+
+is_spike = len(sys.argv) > 2 and sys.argv[2] == 'target'
 
 build_time_start = time.time()
 
 timesteps = 1000
 dt_step = 0.001
-net = nef.Network('Encoder Test', dt=dt_step)
+net = nef.Network('Encoder Test', dt=dt_step,seed=103)
 net.make_input('in1', math.sin)
 net.make_input('in2', math.cos)
 net.make('A', 100, 1)
@@ -20,7 +24,11 @@ net.make('B', 100, 1, encoders=[[1]], intercept=(0, 1.0))
 net.make('C', 100, 2, radius=1.5)
 net.make('D', 100, 2, encoders=[[1,1],[1,-1],[-1,-1],[-1,1]], radius=1.5)
 net.make('outputC', 1, 1, mode='direct')
-net.make('outputD', 1, 1, mode='direct')
+
+if is_spike:
+    net.make('outputD', 1, 1, mode='direct', is_printing=True)
+else:
+    net.make('outputD', 1, 1, mode='direct')
 
 net.connect('in1', 'A')
 net.connect('A', 'B')
@@ -35,34 +43,37 @@ net.connect('D', 'outputD', func=prod)
 
 t = np.linspace(dt_step, timesteps*dt_step, timesteps)
 pstc = 0.01
-I1p = net.make_probe('in1', dt_sample=dt_step, pstc=pstc)
-I2p = net.make_probe('in2', dt_sample=dt_step, pstc=pstc)
-Ap = net.make_probe('A', dt_sample=dt_step, pstc=pstc)
-Bp = net.make_probe('B', dt_sample=dt_step, pstc=pstc)
-Cp = net.make_probe('outputC', dt_sample=dt_step, pstc=pstc)
-Dp = net.make_probe('outputD', dt_sample=dt_step, pstc=pstc)
+#I1p = net.make_probe('in1', dt_sample=dt_step, pstc=pstc)
+#I2p = net.make_probe('in2', dt_sample=dt_step, pstc=pstc)
+#Ap = net.make_probe('A', dt_sample=dt_step, pstc=pstc)
+#Bp = net.make_probe('B', dt_sample=dt_step, pstc=pstc)
+#Cp = net.make_probe('outputC', dt_sample=dt_step, pstc=pstc)
+#Dp = net.make_probe('outputD', dt_sample=dt_step, pstc=pstc)
 
 build_time_end = time.time()
 
 print "starting simulation"
-net.run(timesteps * dt_step)
+if is_spike:
+    net.run(timesteps * dt_step)
+else:
+    net.run(timesteps * dt_step, print_origin='outputD')
 
 sim_time_end = time.time()
-print "\nBuild time: %0.10fs" % (build_time_end - build_time_start)
-print "Sim time: %0.10fs" % (sim_time_end - build_time_end)
+#print "\nBuild time: %0.10fs" % (build_time_end - build_time_start)
+#print "Sim time: %0.10fs" % (sim_time_end - build_time_end)
 
-plt.ioff(); plt.close(); plt.hold(1)
-plt.subplot(211)
-plt.plot(t, I1p.get_data())
-plt.plot(t, Ap.get_data())
-plt.plot(t, Bp.get_data())
-plt.legend(['Input', 'A', 'B'])
-
-plt.subplot(212); plt.title('Multiplication test')
-plt.plot(t, I1p.get_data() * I2p.get_data())
-plt.plot(t, Cp.get_data())
-plt.plot(t, Dp.get_data())
-plt.legend(['Answer', 'C', 'D'])
-plt.tight_layout()
-plt.show()
+#plt.ioff(); plt.close(); plt.hold(1)
+#plt.subplot(211)
+#plt.plot(t, I1p.get_data())
+#plt.plot(t, Ap.get_data())
+#plt.plot(t, Bp.get_data())
+#plt.legend(['Input', 'A', 'B'])
+#
+#plt.subplot(212); plt.title('Multiplication test')
+#plt.plot(t, I1p.get_data() * I2p.get_data())
+#plt.plot(t, Cp.get_data())
+#plt.plot(t, Dp.get_data())
+#plt.legend(['Answer', 'C', 'D'])
+#plt.tight_layout()
+#plt.show()
 
