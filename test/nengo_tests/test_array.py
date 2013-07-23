02e523f39a2b1f1bff3173212adcadf397ae24cc
Greta Cutulenco <gcutulenco@gmail.com>
2013-07-23 18:30:39 -0400

    fix bugs introduced in revert

--- test/nengo_tests/test_array.py
+++ test/nengo_tests/test_array.py
@@ -14,6 +14,8 @@ def test_array():
 
     neurons = 40
 
+    is_spike = len(sys.argv) > 2 and sys.argv[2] == 'target'
+
     net = nef.Network('Array Test', seed=50)
     net.make_input('in', np.arange(-1, 1, .34), zero_after_time=1.0)
     #net.make_input('in', value=1, zero_after=1.0)
@@ -21,7 +23,6 @@ def test_array():
     net.make('B', neurons=neurons, array_size=3, dimensions=2)
     net.make_array('A', neurons=neurons, array_size=1, dimensions=6)
     net.make('A2', neurons=neurons, array_size=2, dimensions=3)
-    net.make('B', neurons=neurons, array_size=3, dimensions=2)
     net.make('B2', neurons=neurons, array_size=6, dimensions=1)
 
     net.connect('in', 'A')
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -17,8 +17,11 @@ TEST_SCRIPTS=(
 );
 
 compareOutput(){
+  # TODO: write a test weapper in python that handles user options. We're
+  # using just "target" as an argument for now, to inficate the we're running
+  # spike code
   ACTUAL_OUT_CMD="${PYTHON} ${THIS_SCRIPT_DIRECTORY}/${1}\
-    ${THIS_SCRIPT_DIRECTORY}/${TARGET_DIR} tt"
+    ${THIS_SCRIPT_DIRECTORY}/${TARGET_DIR} target"
   EXPECTED_OUT_CMD="${PYTHON}  ${THIS_SCRIPT_DIRECTORY}/${1}\
     ${THIS_SCRIPT_DIRECTORY}/${SOURCE_DIR}"
 
