5cfb25327688127b6c6fd1807c6f07514d918ee1
Robert Elder <robert@robertelder.ca>
2013-09-02 23:58:44 -0400

    Added test_noise.py to unit tests

--- test/nengo_tests/test_noise.py
+++ test/nengo_tests/test_noise.py
@@ -3,11 +3,13 @@
 import math
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-from .. import nef_theano as nef
+import sys
+sys.path.append(sys.argv[1])
+import nef_theano as nef
 
-net = nef.Network('Noise Test')
+net = nef.Network('Noise Test',seed=91)
 net.make_input('in', value=math.sin)
 net.make('A', neurons=300, dimensions=1, noise=1)
 net.make('A2', neurons=300, dimensions=1, noise=100)
@@ -33,16 +35,43 @@ Cp = net.make_probe('C', dt_sample=dt_step, pstc=pstc)
 print "starting simulation"
 net.run(timesteps * dt_step)
 
+ip_data = Ip.get_data()
+ap_data = Ap.get_data()
+a2p_data = A2p.get_data()
+bp_data = Bp.get_data()
+cp_data = Cp.get_data()
+
+
+print "input 'in' probe data"
+for x in ip_data:
+    print x
+
+print "input 'ap' probe data"
+for x in ap_data:
+    print x
+
+print "input 'a2p' probe data"
+for x in a2p_data:
+    print x
+
+print "input 'bp' probe data"
+for x in bp_data:
+    print x
+
+print "input 'cp' probe data"
+for x in cp_data:
+    print x
+
 # plot the results
-plt.ioff(); plt.close()
-plt.subplot(411); plt.title('Input')
-plt.plot(Ip.get_data())
-plt.subplot(412); plt.hold(1)
-plt.plot(Ap.get_data()); plt.plot(A2p.get_data())
-plt.legend(['A noise = 1', 'A2 noise = 100'])
-plt.subplot(413); plt.title('B noise = 1000, type = gaussian')
-plt.plot(Bp.get_data())
-plt.subplot(414); plt.title('C')
-plt.plot(Cp.get_data())
-plt.tight_layout()
-plt.show()
+#plt.ioff(); plt.close()
+#plt.subplot(411); plt.title('Input')
+#plt.plot(Ip.get_data())
+#plt.subplot(412); plt.hold(1)
+#plt.plot(Ap.get_data()); plt.plot(A2p.get_data())
+#plt.legend(['A noise = 1', 'A2 noise = 100'])
+#plt.subplot(413); plt.title('B noise = 1000, type = gaussian')
+#plt.plot(Bp.get_data())
+#plt.subplot(414); plt.title('C')
+#plt.plot(Cp.get_data())
+#plt.tight_layout()
+#plt.show()
