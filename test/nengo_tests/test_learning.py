988579776983d8f0190cbfb8caf6e52a3ed8aff0
Robert Elder <robert@robertelder.ca>
2013-09-05 00:08:53 -0400

    Added test_learning.py

--- test/nengo_tests/test_learning.py
+++ test/nengo_tests/test_learning.py
@@ -5,13 +5,14 @@ import math
 import time
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-from .. import nef_theano as nef
+import sys
+sys.path.append(sys.argv[1])
+import nef_theano as nef
+#import matplotlib.pyplot as plt
 
 neurons = 30  # number of neurons in all ensembles
 
-net = nef.Network('Learning Test')
+net = nef.Network('Learning Test',seed=72)
 net.make_input('in', value=0.8)
 timer = time.time()
 net.make('A', neurons=neurons, dimensions=1)
@@ -37,14 +38,35 @@ E1p = net.make_probe('error1', dt_sample=dt_step, pstc=pstc)
 print "starting simulation"
 net.run(t_final)
 
-plt.ioff(); plt.close()
+#plt.ioff(); plt.close()
+
+ip_data = Ip.get_data()
+ap_data = Ap.get_data()
+bp_data = Bp.get_data()
+e1p_data = E1p.get_data()
+
+print "input 'in' probe data"
+for x in ip_data:
+    print x
+
+print "input 'ap' probe data"
+for x in ap_data:
+    print x
+
+print "input 'bp' probe data"
+for x in bp_data:
+    print x
+
+print "input 'e1p' probe data"
+for x in e1p_data:
+    print x
 
 t = np.linspace(0, t_final, len(Ap.get_data()))
 
-plt.plot(t, Ap.get_data())
-plt.plot(t, Bp.get_data())
-plt.plot(t, E1p.get_data())
-plt.legend(['A', 'B', 'error'])
-plt.title('Normal learning')
-plt.tight_layout()
-plt.show()
+#plt.plot(t, Ap.get_data())
+#plt.plot(t, Bp.get_data())
+#plt.plot(t, E1p.get_data())
+#plt.legend(['A', 'B', 'error'])
+#plt.title('Normal learning')
+#plt.tight_layout()
+#plt.show()
