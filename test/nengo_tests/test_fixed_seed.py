15c74e0589b72a9ac1617ae422629ed3e3565a5f
Robert Elder <robert@robertelder.ca>
2013-09-12 23:11:08 -0400

    Added test_fixed_seed.py test.

--- test/nengo_tests/test_fixed_seed.py
+++ test/nengo_tests/test_fixed_seed.py
@@ -4,9 +4,11 @@ identical ensembles.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-from .. import nef_theano as nef
+import sys
+sys.path.append(sys.argv[1])
+import nef_theano as nef
 
 net = nef.Network('Array Test', fixed_seed=5)
 net.make_input('in', [1], zero_after_time=1.0)
@@ -32,16 +34,34 @@ ABp = net.make_probe('AB', dt_sample=dt_step, pstc=pstc)
 print "starting simulation"
 net.run(timesteps*dt_step)
 
+ip_data = Ip.get_data()
+ap_data = Ap.get_data()
+bp_data = Bp.get_data()
+abp_data = ABp.get_data()
+
+print "input 'in' probe data"
+for x in ip_data:
+    print x
+print "'ap' probe data"
+for x in ap_data:
+    print x
+print "'bp' probe data"
+for x in bp_data:
+    print x
+print "'abp' probe data"
+for x in abp_data:
+    print x
+
 # plot the results
-plt.ioff(); plt.close(); 
-plt.subplot(5,1,1)
-plt.plot(t, Ip.get_data(), 'x'); plt.title('Input')
-plt.subplot(5,1,2)
-plt.plot(Ap.get_data()); plt.title('A')
-plt.subplot(5,1,3)
-plt.plot(Bp.get_data()); plt.title('B')
-plt.subplot(5,1,4)
-plt.plot(ABp.get_data()[:,0]); plt.title('AB[0]')
-plt.subplot(5,1,5)
-plt.plot(ABp.get_data()[:,1]); plt.title('AB[1]')
-plt.show()
+#plt.ioff(); plt.close(); 
+#plt.subplot(5,1,1)
+#plt.plot(t, Ip.get_data(), 'x'); plt.title('Input')
+#plt.subplot(5,1,2)
+#plt.plot(Ap.get_data()); plt.title('A')
+#plt.subplot(5,1,3)
+#plt.plot(Bp.get_data()); plt.title('B')
+#plt.subplot(5,1,4)
+#plt.plot(ABp.get_data()[:,0]); plt.title('AB[0]')
+#plt.subplot(5,1,5)
+#plt.plot(ABp.get_data()[:,1]); plt.title('AB[1]')
+#plt.show()
