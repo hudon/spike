bb17299c4a8294dccffe45a8c7e24939654365bf
Robert Elder <robert@robertelder.ca>
2013-12-10 11:20:41 -0500

    Fixed another test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -13,6 +13,7 @@ NENGO_TESTS_DIR="nengo_tests"
 HOSTS_FILE=$1
 
 TEST_SCRIPTS=(
+   "${NENGO_TESTS_DIR}/test_noise.py"
   "${NENGO_TESTS_DIR}/test_array.py"
   # "${NENGO_TESTS_DIR}/test_basal_ganglia.py"
   # "${NENGO_TESTS_DIR}/test_decoded_weight_matrix.py"
@@ -21,7 +22,6 @@ TEST_SCRIPTS=(
   # "${NENGO_TESTS_DIR}/test_eval_points.py"
   # "${NENGO_TESTS_DIR}/test_fixed_seed.py"
   "${NENGO_TESTS_DIR}/test_func.py"
-  # "${NENGO_TESTS_DIR}/test_noise.py"
   # "${NENGO_TESTS_DIR}/test_radius.py"
   # "${NENGO_TESTS_DIR}/test_runtime.py"
   # # "${NENGO_TESTS_DIR}/test_simplenode.py" ## uses instance methods (cannot pickle)
