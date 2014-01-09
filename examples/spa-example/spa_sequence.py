5e1ff78a20d69a5cc728525fe63693c875b33310
James Hudon <jameshudon@gmail.com>
2014-01-09 11:13:49 -0500

    add files in examples

---
+++ examples/spa-example/spa_sequence.py
@@ -0,0 +1,35 @@
+import spa
+import nengo.nef_theano as nef
+
+class Rules:
+    def A(state='A'):
+        effect(state='B')
+    def B(state='B'):
+        effect(state='C')
+    def C(state='C'):
+        effect(state='D')
+    def D(state='D'):
+        effect(state='E')
+    def E(state='E'):
+        effect(state='A')
+    
+class Sequence(spa.SPA):
+    dimensions = 8
+    verbose = True
+    
+    state = spa.Buffer()
+    BG = spa.BasalGanglia(Rules)
+    thal = spa.Thalamus(BG)
+    
+    input = spa.Input(0.1,state='D')
+
+net = nef.Network('Sequence', seed=1)
+seq = Sequence(net)
+
+pThal = net.make_probe('thal.rule', dt_sample=0.001)
+
+net.run(1)
+
+import pylab
+pylab.plot(pThal.get_data())
+pylab.show()
