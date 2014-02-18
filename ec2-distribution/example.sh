8829b5e94784d8666122137f9125c5d02f1195fc
Robert Elder <robert@robertelder.ca>
2014-02-18 00:25:43 +0000

    Stuff to kill daemons.

--- ec2-distribution/example.sh
+++ ec2-distribution/example.sh
@@ -10,16 +10,17 @@ admin_host=${instances[0]}
 
 #  Truncate the old remote hosts file
 ssh -i spike-keypair ubuntu@${admin_host} "cd /home/ubuntu/spike/test/ && :> remote_hosts.txt"
+ssh -i spike-keypair ubuntu@${admin_host} "sudo rm -f /tmp/*"
 
 # fill up the remote hosts file with references to the new nodes
 for (( i=1; i<${#instances[@]}; i++ ))
 do
     echo "Setting up host ${instances[${i}]}."
     #  Kill any previous daemons on this host
-    echo "Killing any previous daemons that might be running."
-    ssh -i spike-keypair ubuntu@${instances[${i}]} " if [ \`ps axf | grep distribution | grep -v grep | wc -l\` -ne 0 ]; then a=\`ps axf | grep distribution | grep -v grep | awk '{print \"kill \" $1}'\`; fi"
+    ssh -i spike-keypair ubuntu@${instances[${i}]} "cd /home/ubuntu/spike && git pull origin ec2-distribution"
+    #ssh -i spike-keypair ubuntu@${instances[${i}]} "/home/ubuntu/spike/ec2-distribution/kill-daemon.sh"
     #  Start the daemon on this host
-    ssh -f -i spike-keypair ubuntu@${instances[${i}]} "cd /home/ubuntu/spike/ && nohup python src/distributiond.py > /dev/null 2>&1 &" > /dev/null
+    #ssh -f -i spike-keypair ubuntu@${instances[${i}]} "cd /home/ubuntu/spike/ && nohup python src/distributiond.py > /dev/null 2>&1 &" > /dev/null
     #  Add this host to the admin's remote hosts file
     ssh -i spike-keypair ubuntu@${admin_host} "cd /home/ubuntu/spike/test/ && echo \"${instances[${i}]}\" >> remote_hosts.txt"
     echo "Started daemon on host ${instances[${i}]} and added a reference to it from the admin (${admin_host})"
---
+++ ec2-distribution/kill-daemon.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ `ps axf | grep distribution | grep -v grep | wc -l` -ne 0 ]
+then
+    echo "Killing previous instances of daemon:"
+    cmds=$(ps axf | grep distribution | grep -v grep | awk '{print "kill " $1}')
+    echo "${cmds}"
+    `${cmds}`
+else
+    echo "No previous instances of daemon found."
+fi
