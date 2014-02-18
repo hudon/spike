69951a3eb8044a1919a8ab2a58411dc4d27b4b6e
Robert Elder <robert@robertelder.ca>
2014-02-18 00:43:37 +0000

    Correction to kill script.

--- ec2-distribution/kill-daemon.sh
+++ ec2-distribution/kill-daemon.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-if [ `ps axf | grep distribution | grep -v grep | wc -l` -ne 0 ]
+if [ `ps axf | grep distributiond.py | grep -v grep | wc -l` -ne 0 ]
 then
     echo "Killing previous instances of daemon:"
-    cmds=$(ps axf | grep distribution | grep -v grep | awk '{print "kill " $1}')
+    cmds=$(ps axf | grep distributiond.py | grep -v grep | awk '{print "kill " $1}')
     echo "${cmds}"
     ${cmds}
 else
