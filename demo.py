3d4f4fc52a94e0bdc4641bd9ffb52dab5170f59d
James Hudon <jameshudon@gmail.com>
2014-03-14 19:01:46 -0400

    add three files to demo script

--- demo.py
+++ demo.py
@@ -1,30 +1,52 @@
 import json
+import numpy
 import matplotlib.pyplot as plt
 
-array = []
+a = []
 with open('/tmp/a') as ins:
     for line in ins:
-        array.append(json.loads(line))
+        a.append(json.loads(line))
+b = []
+with open('/tmp/b') as ins:
+    for line in ins:
+        b.append(json.loads(line))
+inputs = []
+with open('/tmp/input') as ins:
+    for line in ins:
+        inputs.append(json.loads(line))
 
 plt.ioff(); plt.clf(); plt.hold(1);
-plt.legend(['A'])
+
+inputhl, = plt.plot([], [])
+ahl, = plt.plot([], [])
+b0hl, = plt.plot([], [])
+b1hl, = plt.plot([], [])
+b2hl, = plt.plot([], [])
+plt.legend(['Input','A','B0','B1','B2'])
 plt.show(block=False)
 plt.tight_layout()
+
 plt.xlim((0, 500))
 plt.ylim((-1.5, 1.5))
 
-def update_plot(arr):
-    plt.plot(arr)
+
+def update_line(hl, x, new_data):
+    hl.set_xdata(numpy.append(hl.get_xdata(), x))
+    hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
 
 import time
-plot_data = []
 draw = 0
-for x in array:
-    plot_data.append(x)
-    update_plot(plot_data)
+for x in range(len(a)):
+    update_line(ahl, x, a[x][0])
+    update_line(b0hl, x, b[x][0])
+    update_line(b1hl, x, b[x][1])
+    update_line(b2hl, x, b[x][2])
+    update_line(inputhl, x, inputs[x][0])
     draw += 1
-    if draw is 5:
+    if draw is 4:
         plt.draw()
         draw = 0
-    time.sleep(0.005)
+    time.sleep(0.001)
+
+time.sleep(10)
 
